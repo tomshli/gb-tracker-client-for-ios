73208245ac09e9fd820a341abfecb50055295477
Whitecap Canada Inc <support@whitecapcanada.com>
2022-03-21 15:52:17 -0400

    Fixed test

--- Tests/GroupByTrackerTests/GroupByTrackerTests.swift
+++ Tests/GroupByTrackerTests/GroupByTrackerTests.swift
@@ -6,6 +6,6 @@ final class GroupByTrackerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(GroupByTracker().text, "Hello, World!")
+        XCTAssertEqual(GroupByTracker(clientId: "test").text, "Hello, World!")
     }
 }
